608a501546e99323022058192b616e20611ebe54
s10204661 <s10204661@connect.np.edu.sg>
2021-08-09 19:10:45 +0800

    Buggy but complete?
    Buggy housekeeper

--- Assets/Scripts/AI Scripts/Statue.cs
+++ Assets/Scripts/AI Scripts/Statue.cs
@@ -5,21 +5,44 @@ using UnityEngine.AI;
 
 public class Statue : MonoBehaviour
 {
+    /// <summary>
+    /// Assigning the gameObject as an agent
+    /// </summary>
     public NavMeshAgent statueAgent;
 
+    /// <summary>
+    /// Assigning the gameObject as an obstacle
+    /// </summary>
+    public NavMeshObstacle statueObstacle;
+
+    /// <summary>
+    /// Assigning the script to the gameObject
+    /// </summary>
     public GameObject statue;
 
+    /// <summary>
+    /// original statue position
+    /// </summary>
     private Vector3 originalPos;
 
+    /// <summary>
+    /// Current statue position
+    /// </summary>
     private Vector3 statuePos;
 
-
+    /// <summary>
+    /// If true, statue is off original position
+    /// If false, statue is not off original position
+    /// Along the lines of turned on instead of being physically "on"
+    /// </summary>
     public bool statueOn;
 
     // Start is called before the first frame update
     void Start()
     {
+        //assigning respective scripts to the statue
         statueAgent = GetComponent<NavMeshAgent>();
+        statueObstacle = GetComponent<NavMeshObstacle>();
         originalPos = statue.transform.position;
         
     }
@@ -33,10 +56,12 @@ public class Statue : MonoBehaviour
         {
             statueOn = false;
             statueAgent.enabled = false;
+            statueObstacle.enabled = true;
         }
         else if (Vector3.Distance(statuePos, originalPos) > 0.5)
         {
             statueOn = true;
+            statueObstacle.enabled = false;
             
         }
         if (statueAgent.enabled == true)
