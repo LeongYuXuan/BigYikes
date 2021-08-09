cface8eaef523bfc95a404b247e9bb5f02d811c8
s10204661 <s10204661@connect.np.edu.sg>
2021-08-09 18:01:53 +0800

    Housekeeper will now tango with you
    Almost working

--- Assets/Scripts/AI Scripts/Statue.cs
+++ Assets/Scripts/AI Scripts/Statue.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Statue : MonoBehaviour
 {
+    public NavMeshAgent statueAgent;
 
     public GameObject statue;
 
@@ -11,16 +13,15 @@ public class Statue : MonoBehaviour
 
     private Vector3 statuePos;
 
-    [SerializeField]
-    private int statueSpd; 
 
     public bool statueOn;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        statueAgent = GetComponent<NavMeshAgent>();
         originalPos = statue.transform.position;
+        
     }
 
     // Update is called once per frame
@@ -31,10 +32,24 @@ public class Statue : MonoBehaviour
         if (Vector3.Distance(statuePos, originalPos) < 0.5)
         {
             statueOn = false;
+            statueAgent.enabled = false;
         }
         else if (Vector3.Distance(statuePos, originalPos) > 0.5)
         {
             statueOn = true;
+            
+        }
+        if (statueAgent.enabled == true)
+        {
+            statueAgent.SetDestination(originalPos);
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (statueOn == false||other.gameObject.name == "HK")
+        {
+            statueAgent.enabled = true;
         }
     }
 }
