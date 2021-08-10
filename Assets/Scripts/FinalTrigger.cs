d657567611a8d194de99691afb574214e833b4da
LeongYuXuan <S10204536@connect.np.edu.sg>
2021-08-10 19:31:17 +0800

    Add Menu things

---
+++ Assets/Scripts/FinalTrigger.cs
@@ -0,0 +1,26 @@
+/******************************************************************************
+Author: Leong Yu Xuan
+
+Name of Class: GameManager
+
+Description of Class: The final trigger for the game. 
+                        Locks player movement and reveals game finish canvas.
+
+Date Created: 0/06/2021
+******************************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FinalTrigger : MonoBehaviour
+{
+    public GameObject EndCanvas;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameManager.instance.activePlayer.CanMove = false;
+        EndCanvas.SetActive(true);
+
+    }
+}
